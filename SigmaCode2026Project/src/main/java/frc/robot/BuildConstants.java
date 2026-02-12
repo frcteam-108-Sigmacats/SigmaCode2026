8ee034c870d3080c9ba6e06fdba6efa9c4b2325f
nikolas <Nikohawk77@gmail.com>
2026-02-12 13:02:20 -0500

    im not done yet i just want to push this now

--- SigmaCode2026Project/src/main/java/frc/robot/BuildConstants.java
+++ SigmaCode2026Project/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SigmaCode2026Project";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 25;
-  public static final String GIT_SHA = "283c59ccab066a823e93daac422d347053e88f40";
-  public static final String GIT_DATE = "2026-01-12 14:25:03 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-27 23:03:39 EST";
-  public static final long BUILD_UNIX_TIME = 1769573019676L;
+  public static final int GIT_REVISION = 27;
+  public static final String GIT_SHA = "335149273f64a551a473841116e0844edb7caaf4";
+  public static final String GIT_DATE = "2026-02-05 13:49:13 EST";
+  public static final String GIT_BRANCH = "drive2eleboogaloo";
+  public static final String BUILD_DATE = "2026-02-11 16:19:34 EST";
+  public static final long BUILD_UNIX_TIME = 1770844774156L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
---
+++ SigmaCode2026Project/src/main/java/frc/robot/subsystems/drive/ModuleIO.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems.drive;
+
+    import edu.wpi.first.math.geometry.Rotation2d;
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ModuleIO {
+  @AutoLog
+  public static class ModuleInputs {
+    public boolean driveConnected = false;
+    public double drivePositionRad = 0.0;
+    public double driveVelocityRadPerSec = 0.0;
+    public double driveAppliedVolts = 0.0;
+    public double driveCurrentAmps = 0.0;
+
+    public boolean turnConnected = false;
+    public Rotation2d turnPosition = new Rotation2d();
+    public double turnVelocityRadPerSec = 0.0;
+    public double turnAppliedVolts = 0.0;
+    public double turnCurrentAmps = 0.0;
+
+    public double[] odometryTimestamps = new double[] {};
+    public double[] odometryDrivePositionsRad = new double[] {};
+    public Rotation2d[] odometryTurnPositions = new Rotation2d[] {};
+  }
+
+}
