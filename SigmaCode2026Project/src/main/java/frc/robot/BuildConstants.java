f5d7ddf1d1aaa5d2a15fd3f86e22c0cb173722fe
Nik-the-Dex <Nikohawk77@gmail.com>
2026-03-02 13:36:02 -0500

    field oriented drive

--- SigmaCode2026Project/src/main/java/frc/robot/BuildConstants.java
+++ SigmaCode2026Project/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SigmaCode2026Project";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 37;
-  public static final String GIT_SHA = "8d36bcb68ba50088e036ea7d2854715833d871ba";
-  public static final String GIT_DATE = "2026-02-28 14:31:57 EST";
-  public static final String GIT_BRANCH = "drive2eleboogaloo";
-  public static final String BUILD_DATE = "2026-02-28 15:49:59 EST";
-  public static final long BUILD_UNIX_TIME = 1772311799124L;
+  public static final int GIT_REVISION = 39;
+  public static final String GIT_SHA = "6b7a2fac8edd9b92f9298546a62f56fbf9f660d2";
+  public static final String GIT_DATE = "2026-03-01 22:37:02 EST";
+  public static final String GIT_BRANCH = "satdrivetest";
+  public static final String BUILD_DATE = "2026-03-02 12:47:53 EST";
+  public static final long BUILD_UNIX_TIME = 1772473673106L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
