4740103f7998836aa2daf0a6c5ec17f23f92066e
nikolas <Nikohawk77@gmail.com>
2026-03-28 11:44:19 -0400

    Got the correct kS and kV values and tuned PID for drive motor

--- SigmaCode2026Project/src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ SigmaCode2026Project/src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -79,10 +79,10 @@ public class DriveConstants {
       (2 * Math.PI) / 60.0 / driveMotorReduction; // Rotor RPM -> Wheel Rad/Sec
 
   // Drive PID configuration
-  public static final double driveKp = 0.05;
+  public static final double driveKp = 1.4;
   public static final double driveKd = 0.0;
-  public static final double driveKs = 0.0;
-  public static final double driveKv = 0.1;
+  public static final double driveKs = 0.31;
+  public static final double driveKv = 0.08;
   public static final double driveSimP = 0.1;
   public static final double driveSimD = 0.0;
   public static final double driveSimKs = 0.0;
