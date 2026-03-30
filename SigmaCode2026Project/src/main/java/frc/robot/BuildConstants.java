7b5cbe37af3716066d04cbf300c5fd74abfa2fd0
nikolas <Nikohawk77@gmail.com>
2026-03-30 19:08:05 -0400

    updated auto paths

--- SigmaCode2026Project/src/main/java/frc/robot/BuildConstants.java
+++ SigmaCode2026Project/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SigmaCode2026Project";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 129;
-  public static final String GIT_SHA = "70f9216c78842fb633a438a915c793bde73c8c6f";
-  public static final String GIT_DATE = "2026-03-29 12:45:19 EDT";
+  public static final int GIT_REVISION = 130;
+  public static final String GIT_SHA = "128a2c5fd4b50256d50cf533d60f63914a68ea70";
+  public static final String GIT_DATE = "2026-03-30 17:55:38 EDT";
   public static final String GIT_BRANCH = "Limelight/ShiftTimerTesting";
-  public static final String BUILD_DATE = "2026-03-30 17:25:21 EDT";
-  public static final long BUILD_UNIX_TIME = 1774905921420L;
+  public static final String BUILD_DATE = "2026-03-30 18:52:26 EDT";
+  public static final long BUILD_UNIX_TIME = 1774911146279L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
