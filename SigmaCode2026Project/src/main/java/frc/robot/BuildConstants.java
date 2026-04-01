9744075c4d45f8654c6cee5d79246bf2eb1ec5c2
Nik-the-Dex <Nikohawk77@gmail.com>
2026-04-01 14:14:31 -0400

    limelight cam streams inside elastic visable

--- SigmaCode2026Project/src/main/java/frc/robot/BuildConstants.java
+++ SigmaCode2026Project/src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SigmaCode2026Project";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 135;
-  public static final String GIT_SHA = "8cbb81f70d50716d53ee5f66e31da6f200d50694";
-  public static final String GIT_DATE = "2026-04-01 11:25:34 EDT";
+  public static final int GIT_REVISION = 136;
+  public static final String GIT_SHA = "c6981b0ad99ae5b7eb802eb379932bbf187bf20f";
+  public static final String GIT_DATE = "2026-04-01 11:31:35 EDT";
   public static final String GIT_BRANCH = "Limelight/ShiftTimerTesting";
-  public static final String BUILD_DATE = "2026-04-01 11:26:24 EDT";
-  public static final long BUILD_UNIX_TIME = 1775057184117L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-04-01 14:13:29 EDT";
+  public static final long BUILD_UNIX_TIME = 1775067209085L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
