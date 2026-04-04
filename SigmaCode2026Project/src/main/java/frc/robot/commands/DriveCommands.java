86f8075b3789d8348bffa149a940f29d287d3fc2
frcteam-108-Sigmacats <122474103+frcteam-108-Sigmacats@users.noreply.github.com>
2026-04-03 21:16:44 -0400

    Added the 180 degree to fix the issue with red alliance flip

--- SigmaCode2026Project/src/main/java/frc/robot/commands/DriveCommands.java
+++ SigmaCode2026Project/src/main/java/frc/robot/commands/DriveCommands.java
@@ -82,7 +82,7 @@ public class DriveCommands {
                   && DriverStation.getAlliance().get() == Alliance.Red;
           drive.runVelocity(
               ChassisSpeeds.fromFieldRelativeSpeeds(
-                  speeds, isFlipped ? drive.getRotation() : drive.getRotation()));
+                  speeds, isFlipped ? drive.getRotation().plus(Rotation2d.k180deg) : drive.getRotation()));
         },
         drive);
   }
